b5d07741462592c3ce0735adaee18afdee723de2
Jonathan Kerr <jonathandrewkerr@gmail.com>
2021-04-10 22:39:07 +0100

    Add test client fixture

--- conftest.py
+++ conftest.py
@@ -1,2 +1,9 @@
+from runtime.routes import app
 from pytest import fixture
+from chalice.test import Client
 
+
+@fixture(scope='function')
+def test_client():
+    with Client(app) as client:
+        yield client
